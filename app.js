7961f34ac2bcb197ada1f99471f473d53111f7df
Alexander Blankenstein <a.blankenstein@live.com>
2022-02-25 14:48:04 -0500

    reworked dark mode functionality and styling. added nav elements and styling.

--- app.js
+++ app.js
@@ -1,16 +1,83 @@
-'use strict'
+const script_page = true;
 
-const switcher = document.querySelector('.btn');
+function initPage() {
 
-switcher.addEventListener('click', function() {
+    const top = document.querySelector('.top');
+    top.classList.add('low');
+    top.style.minHeight = `calc(${window.innerHeight}px - 2.7em)`;
+    top.insertAdjacentHTML('afterend', pageHTML);
+    top.addEventListener('click', () => scrollToSection('about'));
+    
+    window.onresize = () => {
+        top.style.minHeight = `calc(${window.innerHeight}px - 2.7em)`;
+    }
+
+    const anchors = document.querySelectorAll('.anchor');
+    const navBtns = document.querySelectorAll('.navButtons > button');
+
+    window.onscroll = () => {
+    
+        let activeAnchor = -1;
+        let tempAnchor = -1;
+
+        anchors.forEach((anchor, ix) => {
+            if (activeAnchor === -1) {
+                const rect = anchor.getBoundingClientRect();
+                if (rect.top <= window.innerHeight/2) {
+                    tempAnchor = ix;
+                    if (rect.top > 0) {
+                        activeAnchor = ix;
+                    }
+                }
+            }
+        });
+
+        if (activeAnchor === -1) {
+            activeAnchor = tempAnchor;
+        }
+
+        navBtns.forEach((btn, ix) => {
+            if (ix === activeAnchor) {
+                btn.classList.add('on');
+            } else {
+                btn.classList.remove('on');
+            }
+        });
+    }
+}
+
+function scrollToSection(anchorName) {
+
+    const anchor = document.querySelector(`.anchor_${anchorName}`);
+
+    if (anchor) {
+        document.activeElement.blur();
+        anchor.scrollIntoView({behavior: "smooth", block: "start"});
+    }
+}
+
+function toggleDarkMode(button) {
     document.body.classList.toggle('dark-theme')
 
     var className = document.body.className;
     if(className == "light-theme") {
         this.textContent = "Dark";
+        button.innerHTML = "Dark";
     }
     else {
         this.textContent = "Light";
+        button.innerHTML = "Light";
     }
+}
 
-});
+function sendToResume() {
+    location.href="https://drive.google.com/file/d/1QR0QDSoeBRJZgDDaQwd_9KtQDTTHDu8d/view?usp=sharing";
+}
+
+function clearContact() {
+    const msg = document.querySelector('.contact_msg');
+    msg.innerHTML = '';
+    msg.classList = 'contact_msg';
+
+    document.querySelector('.contact_form').reset();
+}
